46a1afccb1afd6e78771ef84400789745d1d3ee2
Jass1321 <jassyra13216@gmail.com>
2021-10-31 16:44:43 -0500

    Agregar archivos de proyecto.

---
+++ S11_MVVM/S11_MVVM/App.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using S11_MVVM.Views;
+
+namespace S11_MVVM
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new OperationsPage();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
